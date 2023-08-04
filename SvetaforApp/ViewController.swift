5c413098515d49d43a6931fc8aa3df4c5890b21c
Sanjarbek <sanjarbekabdurayimov4@gmail.com>
2023-08-04 15:24:04 +0500

    Initial Commit

---
+++ SvetaforApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SvetaforApp
+//
+//  Created by Sanjarbek Abdurayimov on 04/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
