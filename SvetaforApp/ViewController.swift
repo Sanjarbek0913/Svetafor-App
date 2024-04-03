528d9d82ab40ce964de99c5d79d8f23269c06fb0
Sanjarbek <sanjarbekabdurayimov4@gmail.com>
2024-04-03 14:42:50 +0500

    Commit for Svetafor-App

--- SvetaforApp/ViewController.swift
+++ SvetaforApp/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var redView: UIView!
+    
+    @IBOutlet weak var yellowView: UIView!
+    
+    @IBOutlet weak var greenView: UIView!
 
+    @IBOutlet weak var startBtn: UIButton!
+    var count = 0
+    var brightest: CGFloat = 1
+    var dark: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.alpha = dark
+        yellowView.alpha = dark
+        greenView.alpha = dark
+        startBtn.setTitle("Start", for: .normal)
+        
+        redView.layer.cornerRadius = redView.frame.height / 2
+        yellowView.layer.cornerRadius = yellowView.frame.height / 2
+        greenView.layer.cornerRadius = greenView.frame.height / 2
+        startBtn.layer.cornerRadius = startBtn.frame.height / 2
+        startBtn.clipsToBounds = true
     }
 
 
+    @IBAction func StartBtnPressed(_ sender: UIButton) {
+        
+        switch count{
+        case 0:
+            redView.alpha = brightest
+            greenView.alpha = dark
+            count = 1
+        case 1:
+            yellowView.alpha = brightest
+            redView.alpha = dark
+            count = 2
+        case 2:
+            yellowView.alpha = dark
+            greenView.alpha = brightest
+            count = 0
+        default:
+            break
+        }
+        startBtn.setTitle("Next", for: .normal)
+
+    }
 }
 
